2f2d5972a1f16a262cd12b59f3f4315454615050
amit11612502 <38473703+amit11612502@users.noreply.github.com>
2023-05-27 23:53:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Card name="amit" dis="hheekekfekfekfjjjjekffff"/>
+      <Card name="tima" dis="hheekekfekfekfjjjjekffff"/>
+      <Card name="mit" dis="hheekekfekfekfjjjjekffff"/>
+      <Card name="it" dis="hheekekfekfekfjjjjekffff"/>
+        
+    </div>
+  );
+}
+
+export default App;
